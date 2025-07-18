0ee4a3f5103132f83dea9b10eb3d5b47e0b645b1
Taj Hans <account@tajhans.com>
2025-07-18 11:40:16 -0500

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://camberrobotics.com',
-	vite: {
-		plugins: [tailwindcss()],
+  site: 'https://camberrobotics.com',
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  integrations: [sitemap()],
 });
