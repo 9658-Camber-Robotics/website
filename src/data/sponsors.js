cf3829467737455d4ddb3d529c4e88a42ec193df
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2026-07-31 12:07:40 -0500

    Updated sponsors page, and added frc-tees

---
+++ src/data/sponsors.js
@@ -0,0 +1,13 @@
+import geneHaasImg from "../assets/GeneHaasFoundation.webp";
+import honeywellImg from "../assets/Honeywell.svg";
+import keystoneImg from "../assets/Keystone.png";
+import pineLakeVistasImg from "../assets/PineLakeVistas.png";
+import yassImg from "../assets/YASS.png";
+
+export const sponsors = [
+	{ name: "Gene Haas Foundation", url: "https://www.genehaasfoundation.org", logo: geneHaasImg },
+	{ name: "Honeywell", url: "https://www.honeywell.com", logo: honeywellImg },
+	{ name: "Pine Lake Vistas", url: "https://pinelakevistas.com", logo: pineLakeVistasImg },
+	{ name: "Yet Another Software Suite", url: "https://yetanothersoftwaresuite.com", logo: yassImg },
+	{ name: "Keystone School", url: "https://www.keystoneschool.org", logo: keystoneImg, invert: true },
+];
