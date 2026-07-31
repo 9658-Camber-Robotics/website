9a5d1574ae2766a70edf2e82c9a1f4898bfe19dc
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2026-07-31 12:52:48 -0500

    Updated to include sponsor and business plan

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -8,8 +8,8 @@ import yassImg from "../assets/YASS.png";
 export const sponsors = [
 	{ name: "Gene Haas Foundation", url: "https://www.genehaasfoundation.org", logo: geneHaasImg },
 	{ name: "Honeywell", url: "https://www.honeywell.com", logo: honeywellImg },
-	{ name: "Pine Lake Vistas", url: "https://pinelakevistas.com", logo: pineLakeVistasImg },
+	{ name: "Pine Lake Vistas", url: "https://pinelakevistas.com", logo: pineLakeVistasImg, filterClass: "dark:invert" },
 	{ name: "Yet Another Software Suite", url: "https://yetanothersoftwaresuite.com", logo: yassImg },
-	{ name: "FRC Tees", url: "https://frctees.com", logo: frcTeesImg },
-	{ name: "Keystone School", url: "https://www.keystoneschool.org", logo: keystoneImg, invert: true },
+	{ name: "FRC Tees", url: "https://frctees.com", logo: frcTeesImg, filterClass: "dark:invert" },
+	{ name: "Keystone School", url: "https://www.keystoneschool.org", logo: keystoneImg, filterClass: "invert dark:invert-0" },
 ];
